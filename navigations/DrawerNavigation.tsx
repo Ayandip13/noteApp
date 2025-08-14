9e89a8b8f6b96df3a23706062e51846a4471d9a3
ayandip <ayandip.paul@weavers-web.com>
2025-08-14 15:15:30 +0530

    some better spacings for readability

--- navigations/DrawerNavigation.tsx
+++ navigations/DrawerNavigation.tsx
@@ -8,31 +8,24 @@ const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigator() {
   return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#064232'
-        },
-        headerTintColor: '#FFF5F2'
-      }}>
-      <Drawer.Screen
-        options={{
-          headerTitleAlign: 'center'
-        }}
-        name="Notes"
-        component={HomeScreen} />
-      <Drawer.Screen
-        options={{
-          headerTitleAlign: 'center'
-        }}
-        name="Add Note"
-        component={AddNoteScreen} />
-      <Drawer.Screen
-        options={{
-          headerTitleAlign: 'center'
-        }}
-        name="Profile"
-        component={ProfileScreen} />
+    <Drawer.Navigator screenOptions={{
+      headerStyle: {
+        backgroundColor: '#064232'
+      },
+      headerTintColor: '#FFF5F2'
+    }}>
+      <Drawer.Screen options={{
+        headerTitleAlign: 'center'
+      }}
+        name="Notes" component={HomeScreen} />
+      <Drawer.Screen options={{
+        headerTitleAlign: 'center'
+      }}
+        name="Add Note" component={AddNoteScreen} />
+      <Drawer.Screen options={{
+        headerTitleAlign: 'center'
+      }}
+        name="Profile" component={ProfileScreen} />
     </Drawer.Navigator>
   );
 }
